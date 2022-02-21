e5a8e38b13b05e24fc3eaee4701b18c136514561
Lukas Koch <luk4skoch@gmail.com>
2022-02-21 16:58:17 +0100

    set color by string

---
+++ color_string.py
@@ -0,0 +1,27 @@
+help = "print(f'{color.blue}text') sets color blue."\
+"\nprint(color.bgreen,'text') sets backgroundcolor green. \nprint(color.normal) sets all normal."
+
+normal = "\033[0m"
+
+bold = "\033[1m"
+thin = "\033[2m"
+cursiv = "\033[3m"
+underline = "\033[4m"
+
+gray = "\033[30m"
+red = "\033[31m"
+green = "\033[32m"
+yellow = "\033[33m"
+blue = "\033[34m"
+magenta = "\033[35m"
+cyan = "\033[36m"
+white = "\033[37m"
+
+bblack = "\033[40m"
+bred = "\033[41m"
+bgreen = "\033[42m"
+byellow = "\033[43m"
+bblue = "\033[44m"
+bmagenta = "\033[45m"
+bcyan = "\033[46m"
+bwhite = "\033[47m"
